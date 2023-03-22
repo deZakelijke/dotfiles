f289a20bf31bac5513710f1fe9180022827b970d
Micha de Groot <micha.degroot@braincreators.com>
2023-03-22 09:49:27 +0100

    Added lazygit and lazygit vim plugin

--- setup.sh
+++ setup.sh
@@ -40,6 +40,13 @@ ln -sf "$PWD/files/profile" "$ORIGINAL_PROFILE"
 ln -sf "$PWD/files/vimrc" "$ORIGINAL_VIMRC"
 ln -sf "$PWD/files/vimrc" "$HOME/.vim/init.vim"
 
+# Download and install lazygit
+LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
+curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+tar xf lazygit.tar.gz lazygit
+sudo install lazygit /usr/local/bin
+rm lazygit lazygit.tar.gz
+
 # Install vim-plug and plugins
 printf "\n Installing vim plugins...\n"
 wget -O ~/.config/nvim/autoload/plug.vim \
