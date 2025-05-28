ac44702f08cd831d2ef53ca6dfc77acd4fe42587
Micha de Groot <micha.degroot@braincreators.com>
2025-05-28 11:49:26 +0200

    chore: some cargo stuff in profile ??

--- setup.sh
+++ setup.sh
@@ -78,6 +78,7 @@ sudo npm install -g n
 
 # Copy neovim config
 printf "\nCopying neovim config...\n"
+# TODO: remove `$HOME/.config/nvim` if it exists
 git clone https://github.com/deZakelijke/kickstart.nvim.git "${XDG_CONFIG_HOME:-$HOME/.config}"/nvim
 
 # Read --with arguments
